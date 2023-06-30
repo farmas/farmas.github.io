149564ac2dc36688df1ad65892611318e719c8ac
Federico Silva Armas <farmas@uw.edu>
2023-06-30 09:58:38 -0700

    Added more files for eric

---
+++ eric.js
@@ -0,0 +1,43 @@
+// 1. Variable assign
+var age = 13;
+var name = 'Eric Alonso';
+
+age = 14;
+var maxAge = 50;
+
+console.log(name);
+
+// 2. Conditional (if then else)
+if (age < maxAge && name == 'Eric Silva' && age > 0) {
+  console.log('He is young!');
+  // && AND operator
+  // || OR operator
+
+  // == equals
+  // < less
+  // > greater
+  // <= less or equals
+  // >== greater or equals
+  // != Not equals
+} else {
+  console.log('he is old');
+}
+
+// 3. Loops
+while (age < maxAge) {
+  console.log(age);
+  age = age + 1;
+}
+
+
+// for (variables;condition;increment) {
+
+// }
+
+// for (var x = 0; x < 10; x + 1) {
+
+// }
+
+// 4. Invocation (functions)
+
+
